308156ace66d844fe67668f0b7ae9e6f227dca14
luigitinez <lmgspain@hotmail.com>
2017-07-22 01:39:41 +0200

    Se añade página 404 y se modifica el htacces para que apache redirija a esta pagina cuando ocurra error

---
+++ 404.php
@@ -0,0 +1,32 @@
+<?php 
+      include_once "includes.php";
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+<?php include "modules/head.php";?>
+<link rel="stylesheet"  type="text/css" href="css/login.css">
+<body>
+<?php menu();?>
+<style>
+body {
+  background-color:#ce3635;
+  text-align: center;
+  color:#fff;
+}
+.container{
+      padding-top:10em;
+}
+
+* { color:#fff; text-decoration: none;}
+</style>
+<div class="container">
+    <h1>
+    <a href="" class="typewrite" data-period="2000" data-type='[ "Hola, lo sentimos pero esta página no fue encontrada.", "Error 404.", "Espero que tengas más suerte la próxima.", "Adiós." ]'>
+        <span class="wrap"></span>
+    </a>
+    </h1>
+<div>
+<script type="text/javascript" src="js/404.js"></script>
+</body>
+</html>
